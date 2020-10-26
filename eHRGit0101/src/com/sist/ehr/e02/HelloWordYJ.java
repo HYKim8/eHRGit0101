7d3bb518db55e8e5c4f0069b81c8e6534119f4bf
ffff416 <73327691+ffff416@users.noreply.github.com>
2020-10-26 16:26:16 +0900

    Update HelloWordYJ.java
    branch Test

--- eHRGit0101/src/com/sist/ehr/e02/HelloWordYJ.java
+++ eHRGit0101/src/com/sist/ehr/e02/HelloWordYJ.java
@@ -7,6 +7,7 @@ public class HelloWordYJ {
 		
 		System.out.println("Hello World!");
 		System.out.println("Hello World! by  joun!!!");
+		System.out.println("Hello World! branch & merge Test");
 
 	}
 
