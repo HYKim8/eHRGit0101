7fbeb219cb8471ae1488f52ec007c1bf90aa3058
xp133 <xp133@DESKTOP-NQKEJR3>
2020-10-26 15:11:40 +0900

    new file create

---
+++ eHRGit0101/src/com/sist/ehr/e01/HelloWorldYYM.java
@@ -0,0 +1,10 @@
+package com.sist.ehr.e01;
+
+public class HelloWorldYYM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+	}
+
+}
